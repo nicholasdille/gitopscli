02bd111ca09c3085a4b8b0dfa10f51ae7cc34220
Nicholas Dille <nicholas.dille@mailbox.org>
2020-03-05 15:18:53 +0100

    Changes

--- gitlab.sh
+++ gitlab.sh
@@ -6,3 +6,9 @@ GITLAB_PAT=
 gitlab_branches() {
     curl --silent --header "Private-Token: ${GITLAB_PAT}" https://${GITLAB_HOST}/api/v4/projects/${GITLAB_PROJECT}/repository/branches | jq --raw-output '.[].name'
 }
+
+gitlab_branch_diff() {
+    local from=$1
+    local to=$2
+    curl --silent --header "Private-Token: ${GITLAB_PAT}" "https://${GITLAB_HOST}/api/v4/projects/${GITLAB_PROJECT}/repository/compare?from=${from}&to=${to}&straight=true" | jq --raw-output '.diffs[].diff'
+}
--- k8s.sh
+++ k8s.sh
@@ -8,3 +8,14 @@ k8s_ping() {
     local kubeconfig=$1
     timeout ${K8S_TIMEOUT} kubectl --kubeconfig=${kubeconfig} cluster-info 2>&1 >/dev/null
 }
+
+k8s_get_nodes() {
+    local kubeconfig=$1
+    local params=$2
+    kubectl --kubeconfig=${kubeconfig} get nodes $params
+}
+
+k8s_count_nodes() {
+    local kubeconfig=$1
+    k8s_get_nodes ${kubeconfig} "-o json" | jq '.items | length'
+}
